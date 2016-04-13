c2e0d2c9ee96f844f76f3608fd6c92555e8f18cc
Pedro <plazari96@gmail.com>
2016-04-13 17:44:49 -0300

    Removendo lixos

--- _app/Config.inc.php
+++ _app/Config.inc.php
@@ -26,70 +26,6 @@ define('REQUIRE_PATH', 'themes' . DIRECTORY_SEPARATOR . THEME);
 //sha384, sha512, whirlpool, snefru
 define('Cript', 'sha512');
 
-// URL AMIG�VEL ##############################
-$getUrl = strip_tags(trim(filter_input(INPUT_GET, 'url', FILTER_DEFAULT)));
-$setUrl = (empty($getUrl) ? 'index' : $getUrl);
-$url = explode('/', $setUrl);
-//var_dump($url);
-
-// AUTO LOAD DE CLASSES ############################
-function __autoload($Class) {
-
-    $cDir = ['Conn', 'Helpers', 'Models', 'Own'];
-    $iDir = null;
-
-    foreach ($cDir as $DirName):
-        if (!$iDir && file_exists(__DIR__ . DIRECTORY_SEPARATOR . "{$DirName}" . DIRECTORY_SEPARATOR . "{$Class}.class.php") && !is_dir(__DIR__ . DIRECTORY_SEPARATOR . "{$DirName}" . DIRECTORY_SEPARATOR . "{$Class}.class.php")):
-            include_once (__DIR__ . DIRECTORY_SEPARATOR . "{$DirName}" . DIRECTORY_SEPARATOR . "{$Class}.class.php");
-            $iDir = true;
-        endif;
-    endforeach;
-
-    if (!$iDir):
-        trigger_error("Não foi possivel incluir {$Class}.class.php", E_USER_ERROR);
-        die;
-    endif;
-}
-
-// TRATAMENTO DE ERROS ############################
-// CSS CONSTANTES :: Mensagens de Erros
-define('WS_ACCEPT', 'accept');
-define('WS_INFOR', 'infor');
-define('WS_ALERT', 'alert');
-define('WS_ERROR', 'error');
-
-//WSErro :: Exibe os erros lançados :: Front
-function WSErro($ErrMsg, $ErrNo, $ErrDie = null) {
-    $CssClass = ($ErrNo == E_USER_NOTICE ? WS_INFOR : ($ErrNo == E_USER_WARNING ? WS_ALERT : ($ErrNo == E_USER_ERROR ? WS_ERROR : $ErrNo)));
-    echo "<p class=\"trigger {$CssClass}\">{$ErrMsg}<span class=\"ajax_close\"></span></p>";
-
-    if ($ErrDie):
-        die;
-    endif;
-}
-
-//PHPErro :: personaliza o gatilho do PHP
-function PHPErro($ErrNo, $ErrMsg, $ErrFile, $ErrLine) {
-    $CssClass = ($ErrNo == E_USER_NOTICE ? WS_INFOR : ($ErrNo == E_USER_WARNING ? WS_ALERT : ($ErrNo == E_USER_ERROR ? WS_ERROR : $ErrNo)));
-    echo "<p class=\"trigger {$CssClass}\">";
-    echo "<b>Erro na linha: {$ErrLine} ::</b> {$ErrMsg}<br>";
-    echo "<small>{$ErrFile}</small>";
-    echo "<span class=\"ajax_close\"></span></p>";
-
-    if ($ErrNo == E_USER_ERROR):
-        die;
-    endif;
-}
-
-set_error_handler('PHPErro');
-
-//SISTEMA DE ERRO PADRÃO
-function GeraErro($Erro){
-    if($Erro && is_array($Erro)):
-        echo "<i class={$Erro[0]}>{$Erro[1]}</i>";
-    endif;
-}
-
 //FUNÇÃO PARA MASCARA PHP
 function Mask($mask,$str){
 
@@ -102,26 +38,4 @@ function Mask($mask,$str){
     return $mask;
 
 }
-
-//ENVIO DO SITEMAP
-function SitemapPing() {
-    $SitemapPing = array();
-    $SitemapPing['google'] = 'https://www.google.com/webmasters/tools/ping?sitemap=' . HOME . '/sitemap.xml';
-    $SitemapPing['bing'] = 'https://www.bing.com/webmaster/ping.aspx?siteMap=' . HOME . '/sitemap.xml';
-
-    foreach ($SitemapPing as $SiteMapUrl):
-        $ch = curl_init($SiteMapUrl);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_close($ch);
-    endforeach;
-};
-if(!file_exists('sitemap.xml.gz')):
-    //$gzip = gzopen('sitemap.xml.gz', 'w9');
-    //$gmap = file_get_contents('sitemap.xml');
-    //gzwrite($gzip, $gmap);
-    //gzclose($gzip);
-    // SitemapPing();
-endif;
-?>
 
