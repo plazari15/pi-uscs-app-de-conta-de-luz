42f85a269c4c0fb116f20dc2adc98f2fc78392ba
Pedro Lazari <plazari96@gmail.com>
2016-04-19 13:16:00 -0300

    entregando os templates e mantendo como estava

--- index.php
+++ index.php
@@ -5,19 +5,12 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 require_once ("_app/Config.inc.php");
 
+
 /**
  * Rotas do site
  */
 //Inicia o App
 $App = new \Slim\App();
-
-//Inicia o Container
-$container = $App->getContainer();
-
-//Container pega a pasta do template
-$container['view'] = function ($container) {
-    return new \Slim\Views\PhpRenderer('themes/site/');
-};
 
 
 //Apresentando as rotas do site
@@ -27,11 +20,9 @@ $App->get('/', function () {
 });
 
 
-$App->get('/books/{name}', function ($request, $response, $args) use($container) {
-    return $this->view->render($response, 'index.php', [
-        'name' => $args['name']
-    ]);
-})->setName('index');
+$App->get('/books/{name}', function ($request, $response, $args) {
+    require 'themes/site/index.php';
+});
 
 
 $App->run();
--- themes/site/templates/header.php
+++ themes/site/templates/header.php
@@ -4,6 +4,13 @@
 	<meta charset="UTF-8">
 	<title>Document</title>
 	<link rel="stylesheet" type="text/css" href="assets/font-awesome.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/g/bootstrap@3.3.6(css/bootstrap-theme.min.css+css/bootstrap.min.css)">
 </head>
 <body>
-	
+
+
+
+<script src="assets/jquery.js"></script>
+<script src="assets/algolia.js"></script>
+</body>
+</html>
