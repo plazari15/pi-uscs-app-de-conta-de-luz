46c9c0e8e5ad442479157aac016af8b5967ec8bc
Pedro Lazari <plazari96@gmail.com>
2016-04-19 12:24:29 -0300

    Aplicando os templates ao site.

---
+++ themes/site/templates/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" type="text/css" href="assets/font-awesome.css">
+</head>
+<body>
+	
