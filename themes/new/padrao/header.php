144edb184f794efdc26409f576432a7b0a360b5a
Pedro <plazari96@gmail.com>
2016-05-19 01:34:02 -0300

    Algumas melhorias

--- themes/new/padrao/header.php
+++ themes/new/padrao/header.php
@@ -4,11 +4,19 @@
             <li><a href="<?= HOME ?>" title="Home">Home</a></li>
             <li><a href="<?= HOME ?>/sobre/quem-somos" title="Quem Somos">Quem Somos</a></li>
             <li><a href="<?= HOME ?>/sobre/time" title="Nosso time">Nosso Time</a></li>
+            <li><a href="<?= HOME ?>/sobre/time" title="Nosso time">Ranking da Economia<span class="new badge"></span></a></li>
 <!--            <li><a href="#" title="Como Calcular">Como Calcular<span class="new badge">Em Breve!</span></a></li>-->
             <li><a href="<?= HOME ?>/calculadora" class="waves-effect waves-light btn btn_calcule" title="Calcule">CALCULE!</a></li>
-<!--            <li><a href="#" class="waves-effect waves-light btn btn_login" title="Login">Login!</a></li>-->
+            <a class='waves-effect waves-light btn btn_login dropdown-button btn' href='#' data-activates='dropdown1'>Drop Me!</a>
         </ul>
-
+        <ul id='dropdown1' class='dropdown-content'>
+            <li><a href="#!">Login</a></li>
+            <li><a href="#!">Minha Conta</a></li>
+            <li><a href="#!">Meus Calculos</a></li>
+            <li><a href="#!">Ranking da Economia</a></li>
+            <li class="divider"></li>
+            <li><a href="#!">Sair</a></li>
+        </ul>
         <ul id="nav-mobile" class="side-nav">
             <li><a href="<?= HOME ?>" title="Home">Home</a></li>
             <li><a href="<?= HOME ?>/sobre/quem-somos" title="Quem Somos">Quem Somos</a></li>
